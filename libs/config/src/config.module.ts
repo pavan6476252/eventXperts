b86fbb94a89681fc51d3aa60d610b921a38524fd
Pavan Kumar Meesala <meesalapavan2003@gmail.com>
2024-09-10 20:31:23 +0530

    config package setup

---
+++ libs/config/src/config.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "./config.service";
+
+const configFactory = {
+  provide: ConfigService,
+  useFactory: () => {
+    const config = new ConfigService();
+    config.loadFromEnv();
+    return config;
+  },
+};
+
+@Module({
+  providers: [configFactory],
+  exports: [configFactory],
+})
+export class ConfigModule {}
